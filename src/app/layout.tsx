5fe63b805618980a2a307d9b2275a66f6a48eb6c
MATT VEGAS <matt.vegas@caa.com>
2025-05-24 17:10:51 -0600

    fix bg colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -69,7 +69,7 @@ export default function RootLayout({
         />
       </head>
       <body
-        className={`min-h-screen bg-black text-white dark:bg-black dark:text-white ${inter.className}`}
+        className={`min-h-screen bg-black/99 text-white dark:bg-black/99 dark:text-white ${inter.className}`}
       > <RouteTracker />
         <Header />
         <main className="relative z-10">{children}</main>
